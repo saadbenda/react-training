939f25770553343e3e4c087bc0d226875b53696c
Saad BENDAOUD <saad.benda@gmail.com>
2020-07-17 17:37:29 +0200

    bug with props in edit and add

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,34 +1,94 @@
-import React from "react";
-import './Recipe.css';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import React, { useState } from "react";
+import "./Recipe.css";
+import { ListGroup, ListGroupItem, Input, InputGroup, InputGroupAddon, InputGroupText } from "reactstrap";
 
 import {
   CardImg,
   CardText,
   CardBody,
   CardTitle,
+  Badge,
+  Button,
+  ButtonGroup
 } from "reactstrap";
 
 export function Recipe(props) {
-  console.log("recipe" +props.recipe);
-  const recipe = props.recipe;
+  const [recipe, setRecipe ]= useState(props.recipe);
+  const [editMode, setEditMode ]= useState(false);
+  const [addMode, setAddMode ]= useState(false);
+  const {id, name, picture, description} = props.recipe;
+
+  //const deleteR = props.deleteRecipeF;
+  
+
   return (
     <div className="Recipe">
+      
+      {editMode==false  &&  addMode==false ?
       <CardBody>
-        <CardTitle>{props.recipe.name}</CardTitle>
-        <CardText>{recipe.description}</CardText>
-        <CardImg top src={recipe.picture} alt="Card image cap" />
-        {recipe.ingredients.map(ingredient=>
-        <ListGroup>
-        <ListGroupItem>{ingredient.name}</ListGroupItem>
-        <ListGroupItem>{ingredient.quantity}</ListGroupItem>
-        <ListGroupItem>{ingredient.unit}</ListGroupItem>
-      </ListGroup>
-        )}
+        <CardTitle>{name}</CardTitle>
+        <CardText>{description}</CardText>
+        <CardImg top src={picture} alt="Card image cap" />
+        {recipe.ingredients.map((ingredient) => (
+          <ListGroup>
+            <ListGroupItem>
+              {ingredient.name}{" "}
+              <Badge pill>
+                {ingredient.quantity} {ingredient.unit}
+              </Badge>
+            </ListGroupItem>
+          </ListGroup>
+        ))}
         <CardText>{recipe.instructions}</CardText>
+        <ButtonGroup>
+          <Button onClick={()=>{props.delete(id)}} outline color="primary">Delete</Button>{' '}
+          <Button onClick={()=>{setEditMode(false);setAddMode(true);}} outline color="primary">Add</Button>{' '}
+          <Button onClick={()=>{setEditMode(true);setAddMode(false);}} outline color="primary">Edit</Button>{' '}
+        </ButtonGroup>
       </CardBody>
-     
+      :
+      <CardBody>
+        <InputGroup>
+        <CardTitle>
+          {addMode==false ?
+          <Input defaultValue={name} onChange={(e)=>setRecipe({...recipe, name:e.target.value})}/>
+          
+          :
+          <Input placeholder={name} onChange={(e)=>props.add({...recipe, name:e.target.value})}/>
+        
+          }
+          </CardTitle>
+        <CardText>
+          {addMode==false?
+          <Input type="textarea" defaultValue={description} onChange={(e)=>setRecipe({...recipe, description:e.target.value})}/>
+          :
+          <Input type="textarea" placeholder={description} onChange={(e)=>setRecipe({...recipe, description:e.target.value})}/>
+          }
+          </CardText>
+        <CardText>
+        {addMode==false?
+           <Input defaultValue={picture} onChange={(e)=>setRecipe(e.target.value)}/>
+          :
+          <Input placeholder={picture} onChange={(e)=>setRecipe(e.target.value)}/>
+          }
+          </CardText>
+        <ButtonGroup>
+        {addMode==false ?
+        <Button onClick={()=>{setEditMode(!editMode)}} outline color="primary">Edit</Button>
+        :
+        <Button onClick={()=>{setAddMode(!addMode)}} outline color="primary">Add</Button>
+        }
+        </ButtonGroup>
+
+        
+        
+
+      </InputGroup>
+      </CardBody>
+}
+
     </div>
+    
   );
 }
 export default Recipe;
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Recipes.css";
 import Recipe from "./Recipe";
 import { MOCK } from "./Mock";
 
 export function Recipes(props) {
+  const [recipes, setRecipes] = useState(MOCK);
+  //cosnt [delete, setDelete] = useState(MOCK);
+
+  function deleteRecipe(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
+ function updateRecipe(recipe){
+     deleteRecipe(recipe.id);
+     setRecipes(recipes.push(recipe));
+ }
+
+ function addRecipe(recipe){
+     recipes.push(recipe);
+     console.log('-------add------ '+recipes[recipes.length-1].name);
+ }
+  
+
+  recipes.map((recipe) => console.log(recipe.name));
   return (
     <div className="Recipes">
-      {MOCK.map(recipe => 
-        <Recipe recipe={recipe} />
-      )}
+      {recipes.map((recipe) => (
+        <Recipe recipe={recipe} delete={deleteRecipe} update={updateRecipe} add={addRecipe}/>
+      ))}
     </div>
   );
+
+  // {!editMode && !props.addMode?
+
+  // }
 }
 export default Recipes;
